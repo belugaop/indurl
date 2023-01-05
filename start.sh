915e0bf63c87563e1fc4fa7e763121ad8822f284
Tushar Patil <86295965+PrinceStarLord@users.noreply.github.com>
2023-01-05 03:16:38 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $SOURCE_CODE ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/PrinceStarLord/FLASHLINK-v2.git /FLASHLINK-v2
+  git clone https://github.com/PrinceStarLord/indurl /indurl
 else
   echo "Cloning Custom Repo from $SOURCE_CODE "
-  git clone $SOURCE_CODE /FLASHLINK-v2
+  git clone $SOURCE_CODE /indurl
 fi
-cd /FLASHLINK-v2
+cd /indurl
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 -m main
