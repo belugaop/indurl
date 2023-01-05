604ed99a133e097f3d8b5b84e933e699639cddaf
Tushar Patil <86295965+PrinceStarLord@users.noreply.github.com>
2023-01-05 02:09:05 -0600

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -18,7 +18,7 @@ from utils import extract_link, get_me_button, get_size, getHerokuDetails
 logger = logging.getLogger(__name__)
 
 user_commands = ["mdisk_api", "shortener_api", "header", "footer", "username", "banner_image", "base_site", "me"]
-avl_web = ["flashlink.in"]
+avl_web = ["indiurl.in.net"]
 
 avl_web1 = "".join(f"- {i}\n" for i in avl_web)
 
